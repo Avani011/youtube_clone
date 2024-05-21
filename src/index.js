b353d94f4a8b3af2946d5aabd7807c18c25e060e
Avani011 <102690647+Avani011@users.noreply.github.com>
2024-05-21 14:19:27 +0530

    Updates

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from './reportWebVitals'
+
+import App from "./App";
+
+const root  = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<App />);
