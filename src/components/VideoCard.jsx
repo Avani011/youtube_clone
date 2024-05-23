b61e867880944454912db80b947e46664814290c
Avani011 <102690647+Avani011@users.noreply.github.com>
2024-05-24 03:38:14 +0530

    Changes

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import { Link } from "react-router-dom"; 
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../utils/constants";
 
-const VideoCard = () => {
-  return (
-    <div>
-      
-    </div>
+const VideoCard = ({video: {id: 
+    {videoID}, snippet }}) => {
+
+    return (
+        <Card>
+            <Link to= {videoID ? `/video/${videoID}` : demoVideoUrl}>
+                <CardMedia image={snippet?.thumbnails?.high?.url} 
+                    alt={snippet?.title}
+                    sx={{ width:358, height:180 }}
+                />
+            </Link>
+        </Card>
   );
 }
 
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,21 +1,11 @@
-import { Stack, Box } from "@mui/material";
-import VideoCard from './VideoCard';
-import ChannelCard from './ChannelCard';
+import React from 'react';
 
-const Videos = ({videos}) => {
-
-  if(videos) {
-    return (
-      <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
-        {videos.map((item, idx) => (
-          <Box key={idx}>
-              {item.id.videoID && <VideoCard video={item} />}
-              {item.id.channelID && <ChannelCard channelDetail={item} />}
-          </Box>
-        ))}
-      </Stack>
-    );
-  }
+const Videos = () => {
+  return (
+    <div>
+      
+    </div>
+  );
 }
 
 export default Videos;
