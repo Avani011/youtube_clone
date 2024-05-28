47261a28913d6f80499892b87322cce93f735781
Avani011 <102690647+Avani011@users.noreply.github.com>
2024-05-29 04:35:47 +0530

    Search feed

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -3,7 +3,7 @@ import { CheckCircle } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = ({channelDetail}) => {
+const ChannelCard = ({channelDetail, marginTop}) => {
   return (
     <Box
       sx={{boxShadow: 'none',
@@ -13,7 +13,8 @@ const ChannelCard = ({channelDetail}) => {
       alignItems: 'center',
       width: {xs:'356px', md: '320px'},
       height:'326px',
-      margin: 'auto'
+      margin: 'auto',
+      marginTop,
       }}
     >
       <Link to={`/channel/${channelDetail?.id?.channelID}`} >
--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -5,7 +5,7 @@ import { Box } from "@mui/material";
 import {Videos, ChannelCard} from './';
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
-const ChannelDetail = () => {
+const ChannelDetail = (videos) => {
 
   const [channelDetail, setChannelDetail] = useState(null);
   const [Videos, setVideos] = useState([]);
@@ -17,9 +17,21 @@ const ChannelDetail = () => {
   }, [id])
 
   return (
-    <div>
-      
-    </div>
+    <Box minHeight="95vh" >
+      <Box>
+        <div style={{
+          background: 'Linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,183,1) 100%, rgba(0,212,255,1) 100%)',
+          zIndex: 10,
+          height: '300px'
+        }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+      </Box>
+      <Box display="flex" p="2" >
+        <Box sx={{mr:{sm: '100px'}}} />
+          <Videos videos={videos} />
+      </Box>
+    </Box>
   );
 }
 
