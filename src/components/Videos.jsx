0199d7d50552a19610331811877cd05d0c092992
Avani011 <102690647+Avani011@users.noreply.github.com>
2024-06-09 05:47:10 +0530

    Error Correction
    In this, I look forward to correcting some errors that cause some of my components not to work properly.

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -41,15 +41,17 @@
 import React from "react";
 import { Grid } from "@mui/material";
 import VideoCard from "./VideoCard"; // Assume you have a VideoCard component
+import ChannelCard from "./ChannelCard"; 
 
 const Videos = ({ videos, direction }) => {
   if (!videos?.length) return "Loading...";
 
   return (
     <Grid container direction={direction}>
-      {videos.map((video, idx) => (
+      {videos.map((item, idx) => (
         <Grid item key={idx}>
-          {video.id.videoId ? <VideoCard video={video} /> : null}
+          {item.id.videoId ? <VideoCard video={item} /> : null}
+          {item.id.channelId && <ChannelCard channel={item} />}
         </Grid>
       ))}
     </Grid>
